54740905141c80fe4a2702306f33c05e7fa73b30
pttiming <sean@pttiming.com>
2020-10-22 00:49:23 -0500

    initial functionality to suspend service

--- TrashCollector/Controllers/CustomersController.cs
+++ TrashCollector/Controllers/CustomersController.cs
@@ -88,6 +88,36 @@ namespace TrashCollector.Controllers
                 return View();
             }
         }
+        // GET: CustomerController/Edit/5
+        public ActionResult Suspend(int id)
+        {
+            var customer = _db.Customers.Find(id);
+            return View(customer);
+        }
+
+        // POST: CustomerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Suspend(int id, Customer customer)
+        {
+            try
+            {
+                var customerToUpdate = _db.Customers.Find(id);
+                customerToUpdate.stopPickup = customer.stopPickup;
+                customerToUpdate.restartPickup = customer.restartPickup;
+                if(DateTime.Now >= customer.stopPickup && DateTime.Now < customer.restartPickup)
+                {
+                    customerToUpdate.isActive = false;
+                }
+
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
 
         // GET: CustomerController/Delete/5
         public ActionResult Delete(int id)
